ee006ba210cf85a7d8919fbb5cfecc0f2881927a
Lea Yeh <lea.yeh.ml@gmail.com>
2024-05-13 16:34:07 +0200

    test report

--- tests/DEVlogin.spec.ts
+++ tests/DEVlogin.spec.ts
@@ -5,12 +5,13 @@ test.describe('Login Feature', () => {
     test('DLogin_withValidUserCredentials_dasboardIsDisplayed', async ({page}) => {
         await page.goto('http://109.176.250.87:4200/#/auth/login');
 
-        await page.screenshot({ path: 'playwright-report/screenshot.png' });
 
         await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
         await page.locator('[data-test="password"]').fill('welcome01');
         await page.locator('[data-test="login-submit"]').click();
 
+        await expect(page).toHaveURL("http://109.176.250.87:4200");
+        await page.screenshot({ path: 'playwright-report/screenshot.png' });
     });
 
 });
