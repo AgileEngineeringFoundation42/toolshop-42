575b867966e2d51804486ec88390984ec9b6ba52
leske42 <mhuszar@DESKTOP-004O238>
2024-06-20 11:57:14 +0200

    added tests from ehab and vikas

--- tests/homepage_testing.spec.ts
+++ tests/homepage_testing.spec.ts
@@ -14,3 +14,77 @@ test.describe('Logo test', () => {
     });
 
 });
+
+test('Home_test', async ({ page }) => {
+    // Navigate to the target page
+    await page.goto('http://109.176.250.87:4200/#/');
+  
+    // Locate the element with text "HOME"
+    const homeElement = await page.locator('text=Home');
+  
+    // Assert the element exists and is visible
+    await expect(homeElement).toBeVisible();
+  
+    // Optionally, take a screenshot
+    await page.screenshot({ path: 'playwright-report/home_element.png' });
+  });
+  
+  
+  test('Categories_test', async ({ page }) => {
+    // Navigate to the target page
+    await page.goto('http://109.176.250.87:4200/#/');
+  
+    // Locate the element with text "Catogories"
+    const homeElement = await page.locator('text=Categories');
+  
+    // Assert the element exists and is visible
+    await expect(homeElement).toBeVisible();
+  
+    // Optionally, take a screenshot
+    await page.screenshot({ path: 'playwright-report/home_element.png' });
+  });
+  
+  
+  test('Push_test', async ({ page }) => {
+    // Navigate to the target page
+    await page.goto('http://109.176.250.87:4200/#/');
+  
+    // Locate the element with text "push"
+    const homeElement = await page.locator('text=On push');
+  
+    // Assert the element exists and is visible
+    await expect(homeElement).toBeVisible();
+  
+    // Optionally, take a screenshot
+    await page.screenshot({ path: 'playwright-report/home_element.png' });
+  });
+  
+  
+  test('Signin_test', async ({ page }) => {
+    // Navigate to the target page
+    await page.goto('http://109.176.250.87:4200/#/');
+  
+    // Locate the element with text "Signin"
+    const homeElement = await page.locator('text=Sign in');
+  
+    // Assert the element exists and is visible
+    await expect(homeElement).toBeVisible();
+  
+    // Optionally, take a screenshot
+    await page.screenshot({ path: 'playwright-report/home_element.png' });
+  });
+  
+  
+  test('Sort', async ({ page }) => {
+    // Navigate to the target page
+    await page.goto('http://109.176.250.87:4200/#/');
+  
+    // Locate the element with text "sort"
+    const homeElement = await page.locator('text=Sort');
+  
+    // Assert the element exists and is visible
+    await expect(homeElement).toBeVisible();
+  
+    // Optionally, take a screenshot
+    await page.screenshot({ path: 'playwright-report/home_element.png' });
+  });
